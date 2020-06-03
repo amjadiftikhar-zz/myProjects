81bb6726edb5c2fd284744c831a8c75998469930
amjadiftikhar <amjadiftikhar@github.com>
2020-06-03 17:38:42 +0200

    variables type changed in database file

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,12 +1,13 @@
-let mysql = require("mysql");
+const mysql = require("mysql");
 
 require("dotenv").config();
 const { MYSQL_URL } = process.env;
 
 // var pool = mysql.createConnection(MYSQL_URL);
-let pool = mysql.createPool(MYSQL_URL);
+const pool = mysql.createPool(MYSQL_URL);
 
 module.exports = pool;
+
 
 
 
