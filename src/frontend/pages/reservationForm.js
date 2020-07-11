15c9c09a41eb71d0614d5d22324b5bcbea58d3bb
amjadiftikhar <amjadiftikhar@github.com>
2020-07-11 14:29:06 +0200

    responsive layout has been implemented

--- src/frontend/pages/reservationForm.js
+++ src/frontend/pages/reservationForm.js
@@ -5,16 +5,17 @@ function reservationForm() {
 		<form>
 			<a class = "toAll" href="meals"><< Back</a>
 			<h3>MEAL RESERVATION</h3>
-			<input class = "input" id = "name" type="text" placeholder = "your name!">
-    		<input class = "input" id = "phoneNum" type="number" placeholder = "contact number!"></br>
-    		<input class = "input" id = "email" type="text" placeholder = "your email!">
-			<input class = "input" id = "quantity" type="number" placeholder = "number of guests!"></br>
-			<button id = "sendReservation" onclick="getFields()" >Send</button>
-			<p id="message"></p>
+			<div class="inputfieldClass">
+				<input class = "input" id = "name" type="text" placeholder = "your name!">
+    			<input class = "input" id = "phoneNum" type="number" placeholder = "contact number!"></br>
+    			<input class = "input" id = "email" type="text" placeholder = "your email!">
+				<input class = "input" id = "quantity" type="number" placeholder = "number of guests!"></br>
+				<button id = "sendReservation" onclick="getFields()" >Send</button>
+				<p id="message"></p>
+			</div>
 		</form>;
 	</div>
-	`
+	`;
 }
 
 export default reservationForm;
-
--- src/frontend/pages/reviewForm.js
+++ src/frontend/pages/reviewForm.js
@@ -5,15 +5,17 @@ function renderingReview() {
 		<form>
 			<a class = "toAll" href="meals"><< Back</a>
 			<h3>Review Form</h3>
-			<input class = "input" id = "name" type="text" name="title" placeholder = "enter title!">
-    		<input class = "input" id = "description" type="text" name="description" placeholder = "description!"></br>
-    		<input class = "input" id = "stars" type="number" name="stars" placeholder = "number of stars!">
-			<input class = "input" id = "id" type="number" name="id" placeholder = "meals id!"></br>
-			<button id = "sendReview" onclick="getFields()" >Send</button>
-			<p id="message"></p>
+			<div class="inputfieldClass">
+				<input class = "input" id = "name" type="text" name="title" placeholder = "enter title!">
+    			<input class = "input" id = "description" type="text" name="description" placeholder = "description!"></br>
+    			<input class = "input" id = "stars" type="number" name="stars" placeholder = "number of stars!">
+				<input class = "input" id = "id" type="number" name="id" placeholder = "meals id!"></br>
+				<button id = "sendReview" onclick="getFields()" >Send</button>
+				<p id="message"></p>
+			</div>
 		</form>
 	</div>
 	`;
 }
 
-export default renderingReview
+export default renderingReview;
